1c2a2a802ffd9f690e576e367fa2d96ca5f8c7ab
gevuong <gevuong5627@gmail.com>
2024-07-02 20:29:40 -0700

    test(i18n): generate static params

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
   basePath: isProd ? '/minimal-i18n-with-app-router' : '',
 };
 
