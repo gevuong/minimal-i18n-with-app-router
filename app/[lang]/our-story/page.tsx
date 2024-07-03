2f8dd1913c0bc726f1eaebf5c702600195b4ee1c
gevuong <gevuong5627@gmail.com>
2024-07-03 16:54:47 -0700

    debug(404 WEBP): add optimized images

--- app/[lang]/our-story/page.tsx
+++ app/[lang]/our-story/page.tsx
@@ -1,21 +1,20 @@
 import { Locale } from '@/i18n-config';
-import NextConfig from '@/next.config.mjs';
 import ExportedImage from 'next-image-export-optimizer';
 import { getDictionary } from '../dictionary';
+import MaskHeader from './mask_header.png';
 
 const OurStoryPage = async ({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) => {
-  const { basePath } = NextConfig;
   const dict = await getDictionary(lang);
   return (
     <div>
       <h1>{dict.ourStory.title}</h1>
       <ExportedImage
         className="object-cover"
-        src={`${basePath}/mask_header.png`}
+        src={MaskHeader}
         alt=""
         width={1512}
         height={997}
