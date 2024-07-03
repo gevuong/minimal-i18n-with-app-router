448878743c3d1f6ca487cd5a3e1b1e07bb28e422
gevuong <gevuong5627@gmail.com>
2024-07-02 20:16:00 -0700

    test(i18n): move files to `/[lang]`, add i18n config, dummy metadata, and navbar

---
+++ app/[lang]/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Locale } from '@/i18n-config';
+import Link from 'next/link';
+import { getDictionary } from './dictionary';
+
+const Navbar = ({
+  dict,
+  lang,
+}: {
+  dict: Awaited<ReturnType<typeof getDictionary>>['navbarItems'];
+  lang: Locale;
+}) => {
+  return (
+    <nav>
+      <ul className="flex p-5">
+        {dict.map((item) => (
+          <li className="pr-5" key={item.label}>
+            <Link href={`/${lang}/${item.href}`}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  basePath: isProd ? '/minimal-i18n-with-app-router' : '',
+};
 
 export default nextConfig;
