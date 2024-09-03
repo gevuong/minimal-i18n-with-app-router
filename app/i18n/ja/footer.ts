9d2cf02c21d2f29742014bb8ca99e96c0e6d347a
Anthony Wong <awong88@berkeley.edu>
2024-09-02 19:07:17 -0700

    fix: removed html and dangerouslySetInnerHTML

--- app/i18n/ja/footer.ts
+++ app/i18n/ja/footer.ts
@@ -29,277 +29,61 @@ const footer: Footer = {
         'No recopilamos conscientemente información en línea de niños menores de 18 años. Si se nos notifica que hemos recopilado información personal, tal y como se define en la Ley de Protección de la Privacidad Infantil en Internet ("COPPA"), de un menor de 13 años, eliminaremos la información lo antes posible. Nunca vendemos la información personal de menores de 18 años y no lo haríamos en el futuro sin la autorización afirmativa del consumidor si tiene entre 13 y 16 años, o del padre o tutor de un consumidor menor de 13 años.',
     },
   ],
-  sectionCookiePolicy: [
-    {
-      title: '1. General',
-      content: `
-          La Fundación de Detección de Covid (“Virufy”, “nosotros” o “nuestro”), una corporación sin fines de lucro ubicada en 4546, El Camino Real, B10 #614, Los Altos, código postal 94022, Estados Unidos de América, respeta su privacidad y está comprometida a procesar su información personal de acuerdo con la ley. Esta Política de Cookies (“Política”) explica cómo Virufy utiliza cookies y tecnologías similares para procesar su información personal que usted nos proporciona cuando visita nuestro sitio web (www.virufy.org) (“Sitio Web”) o participa en nuestra encuesta de investigación para la compilación de un conjunto de datos necesario para el desarrollo de algoritmos de inteligencia artificial (IA) para la detección de COVID-19 basada en dispositivos.
-          <div className="mt-4">
-            <p>Para usuarios ubicados en Europa:</p>
-            <li>
-              Esta política de cookies se tratará bajo el Considerando 30 del Reglamento General de Protección de Datos de la Unión Europea 2016/679 (el “GDPR”).
-            </li>
-            <br />
-            <p>Para usuarios ubicados en España:</p>
-            <li>
-              Esta política de cookies se tratará bajo el Considerando 30 del Reglamento General de Protección de Datos de la Unión Europea 2016/679 (el “GDPR”) y bajo el Artículo 22 de la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y Comercio Electrónico (LSSICE).
-            </li>
-            <br />
-            <p>Para usuarios ubicados en Argentina:</p>
-            <li>
-              Esta política de cookies se tratará bajo la Ley Argentina 25.326 de Protección de Datos Personales.
-            </li>
-            <br />
-            <p>Para usuarios ubicados en Brasil:</p>
-            <li>
-              Esta política de cookies se tratará bajo la Ley General de Protección de Datos Personales, número 13.709 / 2018 de Brasil (la “LGPD”).
-            </li>
-            <br />
-            <p>Para usuarios ubicados en Colombia:</p>
-            <li>
-              Esta política de cookies se tratará bajo la Ley colombiana 1581 de 2012.
-            </li>
-            <br />
-            <p>Para usuarios ubicados en los Estados Unidos Mexicanos:</p>
-            <li>
-              Esta política de cookies se considerará parte del Aviso de Privacidad de Virufy y se tratará bajo la Ley Federal de Protección de Datos Personales en Posesión de los Particulares. Siempre que se haga referencia a “Política de Privacidad” en lo presente, el término “Aviso de Privacidad” se entiende de manera intercambiable.
-            </li>
-            <br />
-            <p>Para usuarios ubicados en Bolivia:</p>
-            <li>
-              Esta política de cookies se tratará bajo la Ley Boliviana.
-            </li>
-            <br />
-            <p>Para usuarios ubicados en Perú:</p>
-            <li>
-              Esta política de cookies se tratará bajo la Ley Peruana.
-            </li>
-            <br />
-            <p>
-              Virufy es el “responsable del tratamiento” de, y responsable por, su información personal recolectada de acuerdo con esta Política de Cookies. Para más información sobre cómo utilizamos, almacenamos, divulgamos y procesamos los datos personales que obtenemos a través o en conexión con el uso de nuestro Sitio Web, por favor visite nuestra Política de Privacidad.
-            </p>
-            <br />
-            <p>
-              Cuando acceda por primera vez a nuestro Sitio Web, recibirá un mensaje informándole que se están utilizando cookies y tecnologías similares. Al hacer clic en “Usar solo cookies necesarias” o “Permitir todas las cookies”, usted manifiesta que entiende y acepta el uso de estas tecnologías, como se describe en esta Política de Cookies.
-            </p>
-            <br />
-            <p>
-              Si tiene alguna consulta o queja relacionada con nuestra Política de Cookies, por favor contáctenos por correo electrónico a
-              <a href="mailto:study@virufy.org" class="hover:text-blue-600">study@virufy.org</a>. Nuestro equipo le responderá dentro de los 30 días hábiles siguientes a la recepción de su solicitud.
-            </p>
-          </div>
-        `,
-    },
-    {
-      title: '2. ¿Qué son las Cookies?',
-      content: `
-          <p>Las cookies son archivos de texto que contienen pequeñas piezas de datos que se almacenan en su computadora o dispositivo móvil cada vez que accede a un sitio web. Se utilizan para hacer que los sitios web funcionen o funcionen de manera más eficiente, así como para proporcionar información a los propietarios del sitio web. Las cookies mejoran la experiencia del usuario al permitir que los sitios web las lean y escriban, permitiendo que reconozcan a los visitantes y recuerden información importante que hace que el uso del sitio web sea más conveniente.
-          <div class="mt-4">
-            Las cookies establecidas por un propietario de sitio web (en este caso, Virufy) se denominan “cookies de primera parte”. Las cookies establecidas por terceros ajenos al propietario del sitio web se denominan “cookies de terceros”. Las cookies de terceros permiten que se proporcionen características o funcionalidades de terceros en o a través del Sitio Web (por ejemplo, análisis).
-          </div>
-          </p>
-        `,
-    },
-    {
-      title: '3. ¿Cómo utilizamos las Cookies?',
-      content: `
-          <p>
-            Las cookies son mensajes que los servidores web envían a los navegadores web cuando usted visita sitios web. Las cookies se utilizan comúnmente para rastrear la actividad del sitio web, para rastrear compras en línea, recordar sus preferencias y mejorar su experiencia de usuario a través de páginas web personalizadas.
-            <div class="mt-4">
-              Podemos utilizar los siguientes tipos de Cookies* para hacer que el sitio web sea amigable para el usuario:
-            </div>
-            <div class="mt-4">
-              <li>Cookies Esenciales</li>
-              El Sitio Web puede usar cookies técnicas para llevar a cabo actividades que son estrictamente necesarias para la operación o entrega de servicios. Incluyen, por ejemplo, cookies que le permiten acceder a áreas seguras del sitio web. Utilizamos esta cookie para obtener el consentimiento requerido para procesar datos.
-            </div>
-            <div class="mt-4">
-              <li>Cookies de Preferencia</li>
-              Estas cookies permiten que un sitio web recuerde información que cambia la forma en que el sitio web se comporta o se ve, como el idioma preferido o la región en la que se encuentra.
-            </div>
-            <div class="mt-4">
-              <li>Cookies de Rendimiento</li>
-              También conocidas como cookies de análisis, estas cookies pueden ser utilizadas para recopilar información sobre su uso del Sitio Web y hacer que el Sitio Web sea más amigable para el usuario. El uso de cookies analíticas incluye, pero no se limita a, distinguir usuarios y entender los patrones generales de uso del Sitio Web. Utilizamos esta información en forma agregada para mejorar el funcionamiento de nuestro Sitio Web.
-            </div>
-            <div class="mt-4">
-              <li>Cookies de Marketing</li>
-              Estas cookies se utilizan para rastrear a los visitantes a través de sitios web. La intención es mostrar anuncios que sean relevantes y atractivos para el usuario individual y, por lo tanto, más valiosos para los editores y anunciantes de terceros.
-  
-              Estas cookies se pueden eliminar o bloquear ajustando la configuración de su navegador (ver Sección 4, ¿Cómo puede controlar las Cookies?, a continuación). Alternativamente, siga los enlaces a continuación para optar por no participar: Google Analytics:
-              <a href="https://tools.google.com/dlpage/gaoptout" class="hover:text-blue-600">
-                https://tools.google.com/dlpage/gaoptout
-              </a>
-            </div>
-          </p>
-        `,
-    },
-    {
-      title: '4. ¿Cómo puede controlar las Cookies?',
-      content: `
-          <p>
-            Usted tiene el derecho de decidir si acepta o rechaza cookies. No tiene que aceptar cookies y el consentimiento puede ser retirado en cualquier momento. Tenga en cuenta que las cookies esenciales no se pueden rechazar ya que son estrictamente necesarias para proporcionarle los servicios de nuestro Sitio Web.
-            <div class="mt-4">
-              Utilizando nuestro Banner de Consentimiento de Cookies, puede seleccionar las categorías de cookies que elija aceptar o rechazar. El Banner de Consentimiento de Cookies se encuentra en el banner de notificación y en nuestro Sitio Web. Puede cambiar o revocar el consentimiento para sus políticas de cookies directamente desde el sitio web haciendo clic en “Configuración de cookies” en la parte inferior de la página.
-            </div>
-            <div class="mt-4">
-              Si elige no hacer clic en “¡Entendido!” o no cierra el mensaje, aún puede usar nuestro Sitio Web. También puede rechazar aceptar cookies de este Sitio Web en cualquier momento activando la configuración en su navegador que le permite rechazar cookies.
-            </div>
-            <div class="mt-4">
-              Siga los enlaces a continuación para obtener información útil sobre los navegadores más populares:
-            </div>
-            <div class="mt-4">
-              <li>Google Chrome:
-                <a href="https://support.google.com/chrome/answer/95647?hl=es" class="hover:text-blue-600">https://support.google.com/chrome/answer/95647?hl=es</a>
-              </li>
-              <li>Mozilla Firefox:
-                <a href="https://support.mozilla.org/es/kb/habilitar-y-deshabilitar-cookies-en-firefox" class="hover:text-blue-600">https://support.mozilla.org/es/kb/habilitar-y-deshabilitar-cookies-en-firefox</a>
-              </li>
-              <li>Internet Explorer:
-                <a href="https://support.microsoft.com/es-es/help/278835/how-to-delete-cookie-files-in-internet-explorer" class="hover:text-blue-600">https://support.microsoft.com/es-es/help/278835/how-to-delete-cookie-files-in-internet-explorer</a>
-              </li>
-              <li>Safari:
-                <a href="https://support.apple.com/es-es/HT201265" class="hover:text-blue-600">https://support.apple.com/es-es/HT201265</a>
-              </li>
-              <li>Opera:
-                <a href="https://help.opera.com/en/latest/security-and-privacy/" class="hover:text-blue-600">https://help.opera.com/en/latest/security-and-privacy/</a>
-              </li>
-            </div>
-          </p>
-        `,
-    },
-    {
-      title: '5. Cambios a la Política de Cookies',
-      content: `
-          <p>
-            La Fundación de Detección de Covid puede actualizar esta Política de Cookies ocasionalmente para reflejar cambios en nuestras prácticas o en la ley. Cuando actualicemos nuestra Política de Cookies, modificaremos la fecha de la última actualización en la parte superior de esta página.
-          </p>
-        `,
-    },
-    {
-      title: '6. Contacto',
-      content: `
-          <p>
-            Si tiene alguna pregunta o inquietud sobre esta Política de Cookies, por favor contáctenos a través de:
-          </p>
-          <p>
-            Fundación de Detección de Covid<br />
-            4546, El Camino Real, B10 #614<br />
-            Los Altos, CA 94022<br />
-            Estados Unidos de América<br />
-            <a href="mailto:study@virufy.org" class="hover:text-blue-600">study@virufy.org</a>
-          </p>
-        `,
-    },
-  ],
   sectionPrivacyPolicy: [
     {
       title: 'Argentina',
-      content: `
-        <a href="https://drive.google.com/file/d/1Fef7l77QP3EZboej6mw_q7fBNrayvs03/view" class="hover:text-blue-600">
-          <p>
-            Política de Privacidad
-          </p>
-        </a>
-      `,
+      content: 'Política de Privacidad',
+      link: 'https://drive.google.com/file/d/1Fef7l77QP3EZboej6mw_q7fBNrayvs03/view',
     },
     {
       title: 'Bolivia',
-      content: `
-        <a href="https://drive.google.com/file/d/1ZSrRivk4YJyyDioqYeAsz0fgNAP3UwVn/view" class="hover:text-blue-600">
-          <p>
-            Política de Privacidad
-          </p>
-        </a>
-      `,
+      content: 'Política de Privacidad',
+      link: 'https://drive.google.com/file/d/1ZSrRivk4YJyyDioqYeAsz0fgNAP3UwVn/view',
     },
     {
       title: 'Brazil',
-      content: `
-        <a href="https://drive.google.com/file/d/1Zl9QO-Dliik84xjuXCdFGcDOk5GytXr1/view" class="hover:text-blue-600">
-          <p>
-            Política de Privacidade
-          </p>
-        </a>
-      `,
+      content: 'Política de Privacidade',
+      link: 'https://drive.google.com/file/d/1Zl9QO-Dliik84xjuXCdFGcDOk5GytXr1/view',
     },
     {
       title: 'Colombia',
-      content: `
-        <a href="https://drive.google.com/file/d/1eQOx81C5e5CoESYsEnrE9sWhCkTMDPEq/view" class="hover:text-blue-600">
-          <p>
-            Política de Privacidad
-          </p>
-        </a>
-      `,
+      content: 'Política de Privacidad',
+      link: 'https://drive.google.com/file/d/1eQOx81C5e5CoESYsEnrE9sWhCkTMDPEq/view',
     },
     {
       title: 'Greece',
-      content: `
-        <a href="https://drive.google.com/file/d/1n8YZTb3bAqLLL5ygVE8-MstdzECcKJ_b/view" class="hover:text-blue-600">
-          <p>
-            Πολιτική απορρήτου
-          </p>
-        </a>
-      `,
+      content: 'Πολιτική απορρήτου',
+      link: 'https://drive.google.com/file/d/1n8YZTb3bAqLLL5ygVE8-MstdzECcKJ_b/view',
     },
     {
       title: 'Mexico',
-      content: `
-        <a href="https://drive.google.com/file/d/1Qr3cFttfhjUoKxLcQrVEhsuO-OJ6VE0_/view" class="hover:text-blue-600">
-          <p>
-            Aviso De Privacidad
-          </p>
-        </a>
-      `,
+      content: 'Aviso De Privacidad',
+      link: 'https://drive.google.com/file/d/1Qr3cFttfhjUoKxLcQrVEhsuO-OJ6VE0_/view',
     },
     {
       title: 'Peru',
-      content: `
-        <a href="https://drive.google.com/file/d/1UgN2Bu2XIvv2YirLRYloSPlbYln_uzf9/view" class="hover:text-blue-600">
-          <p>
-            Política de Privacidad
-          </p>
-        </a>
-      `,
+      content: 'Política de Privacidad',
+      link: 'https://drive.google.com/file/d/1UgN2Bu2XIvv2YirLRYloSPlbYln_uzf9/view',
     },
     {
       title: 'United States',
-      content: `
-        <a href="https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view" class="hover:text-blue-600">
-          <p>
-            Privacy Policy
-          </p>
-        </a>
-      `,
+      content: 'Privacy Policy',
+      link: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
     },
     {
       title: 'Europe',
-      content: `
-        <a href="https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view" class="hover:text-blue-600">
-          <p>
-            Privacy Policy
-          </p>
-        </a>
-      `,
+      content: 'Privacy Policy',
+      link: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
     },
     {
       title: 'Japan',
-      content: `
-        <a href="https://drive.google.com/file/d/11RTMmnHW4SqHNf7htr3xuSi-o2v2vWlp/view" class="hover:text-blue-600">
-          <p>
-            プライバシーポリシー
-          </p>
-        </a>
-      `,
+      content: 'プライバシーポリシー',
+      link: 'https://drive.google.com/file/d/11RTMmnHW4SqHNf7htr3xuSi-o2v2vWlp/view',
     },
     {
       title: 'Other Countries',
-      content: `
-        <a href="https://drive.google.com/file/d/1fUIe6FBss6WrwsrssVr6OM_2uFFVhESD/view" class="hover:text-blue-600">
-          <p>
-            Privacy Policy
-          </p>
-        </a>
-      `,
+      content: 'Privacy Policy',
+      link: 'https://drive.google.com/file/d/1fUIe6FBss6WrwsrssVr6OM_2uFFVhESD/view',
     },
   ],
 };
--- app/i18n/types/footer.ts
+++ app/i18n/types/footer.ts
@@ -1,20 +1,16 @@
 export interface Footer {
   sectionPersonalInformation: SectionPersonalInformation[];
-  sectionCookiePolicy: SectionCookiePolicy[];
   sectionPrivacyPolicy: SectionPrivacyPolicy[];
 }
 
 export interface SectionPersonalInformation {
   title: string;
   content: string;
-}
-
-export interface SectionCookiePolicy {
-  title: string;
-  content: string;
+  link?: string;
 }
 
 export interface SectionPrivacyPolicy {
   title: string;
   content: string;
+  link: string;
 }
