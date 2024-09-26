789eb88bf3692f10287ed76c05cbad5e2b870ae4
Anthony Wong <awong88@berkeley.edu>
2024-09-26 12:03:24 -0700

    fix: add icon my-auto

--- app/[lang]/supporters/page.tsx
+++ app/[lang]/supporters/page.tsx
@@ -42,7 +42,7 @@ const SupportersPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
             {/* Supporters mobile view */}
             <div className="flex w-full flex-wrap justify-center">
               {supportersList.map(({ img, alt, link }) => (
-                <div className="mx-1 flex basis-1/4 justify-center" key={alt}>
+                <div className="mx-1 flex basis-1/4 justify-center my-auto" key={alt}>
                   <a href={link}>
                     <ExportedImage
                       src={img}
@@ -63,7 +63,7 @@ const SupportersPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
             {/* Supporters */}
             <div className="flex w-full flex-wrap justify-center">
               {supportersList.map(({ img, alt, link }) => (
-                <div className="mx-0 flex basis-1/4 justify-center" key={alt}>
+                <div className="mx-0 flex basis-1/4 justify-center my-auto" key={alt}>
                   <a href={link}>
                     <ExportedImage
                       src={img}
