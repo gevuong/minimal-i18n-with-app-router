12f15172fbd7ad23d40854fa82b813f73f8b840b
gevuong <gevuong5627@gmail.com>
2024-10-01 15:50:09 -0700

    fix: replace faq and footer metadata structure to account for link types

--- app/i18n/en/footer.ts
+++ app/i18n/en/footer.ts
@@ -4,86 +4,170 @@ const footer: Footer = {
   sectionPersonalInformation: [
     {
       question: 'California Consumer Privacy Act Rights',
-      answer:
-        'California consumers have a right to knowledge, access, and deletion of their personal information under the California Consumer Privacy Act. California consumers also have a right to opt out of the sale of their personal information by a business and a right not to be discriminated against for exercising one of their California privacy rights. Virufy does not sell the personal information of California consumers and does not discriminate in response to privacy rights requests.',
+      answer: [
+        {
+          type: 'text',
+          text: 'California consumers have a right to knowledge, access, and deletion of their personal information under the California Consumer Privacy Act. California consumers also have a right to opt out of the sale of their personal information by a business and a right not to be discriminated against for exercising one of their California privacy rights. Virufy does not sell the personal information of California consumers and does not discriminate in response to privacy rights requests.',
+        },
+      ],
     },
     {
       question:
         'Virufy provides notice of our privacy practices in our Privacy Policy',
-      answer:
-        'The Privacy Policy includes what personal information and data is collected, the source of the personal information and data collected, how personal information and data is processed and stored, and the purposes of use of personal information. Virufy also discloses what personal information and if so, the categories of third parties to whom data and personal information is disclosed. More information about how Virufy handles personal information in connection with specific Virufy services is presented in service-specific notices at or before your first use of the Virufy web app and available at any time. To view a copy of the latest versions of the service-specific privacy notices and the California-specific disclosures, please see our Privacy Policy.',
+      answer: [
+        {
+          type: 'text',
+          text: 'The Privacy Policy includes what personal information and data is collected, the source of the personal information and data collected, how personal information and data is processed and stored, and the purposes of use of personal information. Virufy also discloses what personal information and if so, the categories of third parties to whom data and personal information is disclosed. More information about how Virufy handles personal information in connection with specific Virufy services is presented in service-specific notices at or before your first use of the Virufy web app and available at any time. To view a copy of the latest versions of the service-specific privacy notices and the California-specific disclosures, please see our Privacy Policy.',
+        },
+      ],
     },
     {
       question: 'How to Exercise the Above Rights',
-      answer:
-        'California consumers can exercise their rights to access or delete their information directly or through an authorized agent by contacting Virufy at study@virufy.org. Virufy will ask you for information which we consider necessary to verify your identity for security and to prevent fraud. This information may include name, contact information, and information related to your relationship with Virufy, but the specific information requested may differ depending on the circumstances of your request for your security and to protect privacy rights. If we delete your personal information, we will both render certain personal information about you permanently unrecoverable and also de-identify certain personal information.',
+      answer: [
+        {
+          type: 'text',
+          text: 'California consumers can exercise their rights to access or delete their information directly or through an authorized agent by contacting Virufy at ',
+        },
+        {
+          type: 'link',
+          text: 'study@virufy.org',
+          href: 'mailto:study@virufy.org',
+        },
+        {
+          type: 'text',
+          text: '. Virufy will ask you for information which we consider necessary to verify your identity for security and to prevent fraud. This information may include name, contact information, and information related to your relationship with Virufy, but the specific information requested may differ depending on the circumstances of your request for your security and to protect privacy rights. If we delete your personal information, we will both render certain personal information about you permanently unrecoverable and also de-identify certain personal information.',
+        },
+      ],
     },
     {
       question: 'Direct Marketing and Do Not Track',
-      answer:
-        'Virufy does not track its customers over time and across third party websites to provide targeted advertising and therefore does not respond to web browsers “do not track” signals. If you arrive at our website by way of a link from a third party site that does respond to “do not track” requests, the recognition of any “do not track” request you have initiated will end as soon as you reach our website. Third parties cannot collect any other personally identifiable information from Virufy’s websites unless you provide it to them directly. This is also described in our Privacy Policy.',
+      answer: [
+        {
+          type: 'text',
+          text: 'Virufy does not track its customers over time and across third party websites to provide targeted advertising and therefore does not respond to web browsers “do not track” signals. If you arrive at our website by way of a link from a third party site that does respond to “do not track” requests, the recognition of any “do not track” request you have initiated will end as soon as you reach our website. Third parties cannot collect any other personally identifiable information from Virufy’s websites unless you provide it to them directly. This is also described in our Privacy Policy.',
+        },
+      ],
     },
     {
       question: 'Notice for Minors',
-      answer:
-        'We do not knowingly collect online information from children under the age of 18. If we are notified that we have collected personal information, as defined by the Children’s Online Privacy Protection Act (“COPPA”), of a child under the age of 13, we will delete the information as expeditiously as possible. We never sell the personal information of minors under 18 years of age and would not do so in the future without affirmative authorization of the consumer if between 13 to 16 years of age, or the parent or guardian of a consumer less than 13 years of age.',
+      answer: [
+        {
+          type: 'text',
+          text: 'We do not knowingly collect online information from children under the age of 18. If we are notified that we have collected personal information, as defined by the Children’s Online Privacy Protection Act (“COPPA”), of a child under the age of 13, we will delete the information as expeditiously as possible. We never sell the personal information of minors under 18 years of age and would not do so in the future without affirmative authorization of the consumer if between 13 to 16 years of age, or the parent or guardian of a consumer less than 13 years of age.',
+        },
+      ],
     },
   ],
   sectionPrivacyPolicy: [
     {
       question: 'Argentina',
-      answer: 'Política de Privacidad',
-      link: 'https://drive.google.com/file/d/1Fef7l77QP3EZboej6mw_q7fBNrayvs03/view',
+      answer: [
+        {
+          type: 'text',
+          text: 'Política de Privacidad',
+          href: 'https://drive.google.com/file/d/1Fef7l77QP3EZboej6mw_q7fBNrayvs03/view',
+        },
+      ],
     },
     {
       question: 'Bolivia',
-      answer: 'Política de Privacidad',
-      link: 'https://drive.google.com/file/d/1ZSrRivk4YJyyDioqYeAsz0fgNAP3UwVn/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Política de Privacidad',
+          href: 'https://drive.google.com/file/d/1ZSrRivk4YJyyDioqYeAsz0fgNAP3UwVn/view',
+        },
+      ],
     },
     {
       question: 'Brazil',
-      answer: 'Política de Privacidade',
-      link: 'https://drive.google.com/file/d/1Zl9QO-Dliik84xjuXCdFGcDOk5GytXr1/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Política de Privacidade',
+          href: 'https://drive.google.com/file/d/1Zl9QO-Dliik84xjuXCdFGcDOk5GytXr1/view',
+        },
+      ],
     },
     {
       question: 'Colombia',
-      answer: 'Política de Privacidad',
-      link: 'https://drive.google.com/file/d/1eQOx81C5e5CoESYsEnrE9sWhCkTMDPEq/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Política de Privacidad',
+          href: 'https://drive.google.com/file/d/1eQOx81C5e5CoESYsEnrE9sWhCkTMDPEq/view',
+        },
+      ],
     },
     {
       question: 'Greece',
-      answer: 'Πολιτική απορρήτου',
-      link: 'https://drive.google.com/file/d/1n8YZTb3bAqLLL5ygVE8-MstdzECcKJ_b/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Πολιτική απορρήτου',
+          href: 'https://drive.google.com/file/d/1n8YZTb3bAqLLL5ygVE8-MstdzECcKJ_b/view',
+        },
+      ],
     },
     {
       question: 'Mexico',
-      answer: 'Aviso De Privacidad',
-      link: 'https://drive.google.com/file/d/1Qr3cFttfhjUoKxLcQrVEhsuO-OJ6VE0_/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Aviso De Privacidad',
+          href: 'https://drive.google.com/file/d/1Qr3cFttfhjUoKxLcQrVEhsuO-OJ6VE0_/view',
+        },
+      ],
     },
     {
       question: 'Peru',
-      answer: 'Política de Privacidad',
-      link: 'https://drive.google.com/file/d/1UgN2Bu2XIvv2YirLRYloSPlbYln_uzf9/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Política de Privacidad',
+          href: 'https://drive.google.com/file/d/1UgN2Bu2XIvv2YirLRYloSPlbYln_uzf9/view',
+        },
+      ],
     },
     {
       question: 'United States',
-      answer: 'Privacy Policy',
-      link: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Privacy Policy',
+          href: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+        },
+      ],
     },
     {
       question: 'Europe',
-      answer: 'Privacy Policy',
-      link: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Privacy Policy',
+          href: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+        },
+      ],
     },
     {
       question: 'Japan',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/11RTMmnHW4SqHNf7htr3xuSi-o2v2vWlp/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/11RTMmnHW4SqHNf7htr3xuSi-o2v2vWlp/view',
+        },
+      ],
     },
     {
       question: 'Other Countries',
-      answer: 'Privacy Policy',
-      link: 'https://drive.google.com/file/d/1fUIe6FBss6WrwsrssVr6OM_2uFFVhESD/view',
+      answer: [
+        {
+          type: 'link',
+          text: 'Privacy Policy',
+          href: 'https://drive.google.com/file/d/1fUIe6FBss6WrwsrssVr6OM_2uFFVhESD/view',
+        },
+      ],
     },
   ],
 };
