d28a1dfd19edffa03fb06e554b4c358a6cf016fe
gevuong <gevuong5627@gmail.com>
2024-10-01 15:38:54 -0700

    fix: edit text in ai page, refer to https://github.com/virufy6/virufy6.github.io/pull/51/files for more info

--- app/i18n/ja/ai.ts
+++ app/i18n/ja/ai.ts
@@ -26,8 +26,8 @@ const ai: Ai = {
       },
       {
         title: '02',
-        text: 'Examina varios elementos de la tos, como la duración, la frecuencia y la intensidad. Aprovechando una base de datos completa, Al compara meticulosamente los patrones de tos registrados con indicadores conocidos de enfermedades respiratorias, lo que permite la detección temprana de posibles problemas de salud.',
-        style: '',
+        text: 'El algoritmo examina varios elementos de la tos, como la duración, la frecuencia y la intensidad. Aprovechando una base de datos completa, Al compara meticulosamente los patrones de tos registrados con indicadores conocidos de enfermedades respiratorias, lo que permite la detección temprana de posibles problemas de salud.',
+        style: 'text-[2.5rem] sm:text-6xl',
       },
     ],
   },
