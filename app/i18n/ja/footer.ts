2951c57ac5e82782cd223ff6c1e1cb9123247f37
Chiho Okada <113386829+chihookada@users.noreply.github.com>
2024-10-06 15:19:53 +0000

    Update file structures

--- app/i18n/ja/footer.ts
+++ app/i18n/ja/footer.ts
@@ -4,86 +4,161 @@ const footer: Footer = {
   sectionPersonalInformation: [
     {
       question: 'カリフォルニア州消費者プライバシー法の権利',
-      answer:
-        'カリフォルニア州の消費者は、カリフォルニア州消費者プライバシー法に基づき、個人情報の知識、アクセス、削除の権利を持っています。また、カリフォルニア州の消費者には、自分の個人情報の販売を企業に拒否する権利、およびプライバシー権を行使したことによって差別されない権利があります。Virufyは、カリフォルニア州の消費者の個人情報を販売することはなく、プライバシー権のリクエストに対して差別することもありません。',
+      answer:[
+        {
+          type: 'text',
+          text: 'カリフォルニア州の消費者は、カリフォルニア州消費者プライバシー法に基づき、個人情報の知識、アクセス、削除の権利を持っています。また、カリフォルニア州の消費者には、自分の個人情報の販売を企業に拒否する権利、およびプライバシー権を行使したことによって差別されない権利があります。Virufyは、カリフォルニア州の消費者の個人情報を販売することはなく、プライバシー権のリクエストに対して差別することもありません。',
+        }
+      ]
     },
     {
       question:
         'Virufyのプライバシー慣行に関する通知はプライバシーポリシーで提供されています',
-      answer:
-        'プライバシーポリシーには、収集される個人情報やデータの種類、収集元、個人情報やデータの処理および保存方法、使用目的が記載されています。また、Virufyが個人情報を開示する場合、そのカテゴリーや第三者についても開示されます。特定のVirufyのサービスに関連してVirufyが個人情報をどのように扱うかについての詳細は、Virufyウェブアプリの最初の使用時または使用前に提供され、いつでも参照可能です。サービスごとのプライバシー通知およびカリフォルニア州向けの開示の最新版は、プライバシーポリシーでご確認ください。',
+      answer:[
+        {
+          type: 'text',
+          text: 'プライバシーポリシーには、収集される個人情報やデータの種類、収集元、個人情報やデータの処理および保存方法、使用目的が記載されています。また、Virufyが個人情報を開示する場合、そのカテゴリーや第三者についても開示されます。特定のVirufyのサービスに関連してVirufyが個人情報をどのように扱うかについての詳細は、Virufyウェブアプリの最初の使用時または使用前に提供され、いつでも参照可能です。サービスごとのプライバシー通知およびカリフォルニア州向けの開示の最新版は、プライバシーポリシーでご確認ください。',
+        }
+      ]
     },
     {
       question: '上記の権利を行使する方法',
-      answer:
-        'カリフォルニア州の消費者は、直接または代理人を通じて、自分の情報にアクセスしたり削除したりする権利を行使できます。Virufyへはstudy@virufy.orgに連絡することで可能です。Virufyは、セキュリティと詐欺防止のために、あなたの身元を確認するために必要と考える情報を要求します。この情報には、名前、連絡先情報、Virufyとの関係に関連する情報が含まれる場合がありますが、リクエストの状況によって要求される具体的な情報は異なる場合があります。あなたの個人情報を削除する場合、特定の個人情報を永久に回復不可能な状態にし、また特定の個人情報を識別不能な状態にします。',
+      answer:[
+        {
+          type: 'text',
+          text: 'カリフォルニア州の消費者は、直接または代理人を通じて、自分の情報にアクセスしたり削除したりする権利を行使できます。Virufyへはstudy@virufy.orgに連絡することで可能です。Virufyは、セキュリティと詐欺防止のために、あなたの身元を確認するために必要と考える情報を要求します。この情報には、名前、連絡先情報、Virufyとの関係に関連する情報が含まれる場合がありますが、リクエストの状況によって要求される具体的な情報は異なる場合があります。あなたの個人情報を削除する場合、特定の個人情報を永久に回復不可能な状態にし、また特定の個人情報を識別不能な状態にします。',
+        }
+      ]
     },
     {
       question: 'ダイレクトマーケティングとトラッキング',
-      answer:
-        'Virufyは、顧客を第三者のウェブサイトを通じて時間とともに追跡し、ターゲティング広告を提供することは行っていないため、ウェブブラウザの「Do Not Track」シグナルには対応していません。当団体のウェブサイトを第三者サイトのリンクから開いた場合、その「Do Not Track」リクエストの認識は、当団体のウェブサイトに到着した時点で終了します。第三者がVirufyのウェブサイトから個人を識別できる情報を収集することは、あなたが直接提供しない限りできません。これについては、プライバシーポリシーでも説明されています。',
+      answer:[
+        {
+          type: 'text',
+          text: 'Virufyは、顧客を第三者のウェブサイトを通じて時間とともに追跡し、ターゲティング広告を提供することは行っていないため、ウェブブラウザの「Do Not Track」シグナルには対応していません。当団体のウェブサイトを第三者サイトのリンクから開いた場合、その「Do Not Track」リクエストの認識は、当団体のウェブサイトに到着した時点で終了します。第三者がVirufyのウェブサイトから個人を識別できる情報を収集することは、あなたが直接提供しない限りできません。これについては、プライバシーポリシーでも説明されています。',
+        }
+      ]
     },
     {
       question: '未成年者に関する通知',
-      answer:
-        '当団体は、18歳未満の子供からオンラインで情報を故意に収集することはありません。もし13歳未満の子供の個人情報（COPPAに定義される）を収集したことが通知された場合、その情報は可能な限り迅速に削除されます。また、18歳未満の未成年者の個人情報を販売することはなく、将来的に13～16歳の消費者や13歳未満の消費者の親または保護者の明確な許可がない限り、そうすることはありません。',
+      answer:[
+        {
+          type: 'text',
+          text: '当団体は、18歳未満の子供からオンラインで情報を故意に収集することはありません。もし13歳未満の子供の個人情報（COPPAに定義される）を収集したことが通知された場合、その情報は可能な限り迅速に削除されます。また、18歳未満の未成年者の個人情報を販売することはなく、将来的に13～16歳の消費者や13歳未満の消費者の親または保護者の明確な許可がない限り、そうすることはありません。',
+        }
+      ]
     },
   ],
   sectionPrivacyPolicy: [
     {
       question: 'アルゼンチン',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1Fef7l77QP3EZboej6mw_q7fBNrayvs03/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1Fef7l77QP3EZboej6mw_q7fBNrayvs03/view',
+        }
+      ],
     },
     {
       question: 'ボリビア',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1ZSrRivk4YJyyDioqYeAsz0fgNAP3UwVn/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1ZSrRivk4YJyyDioqYeAsz0fgNAP3UwVn/view',
+        }
+      ],
     },
     {
       question: 'ブラジル',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1Zl9QO-Dliik84xjuXCdFGcDOk5GytXr1/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1Zl9QO-Dliik84xjuXCdFGcDOk5GytXr1/view',
+        }
+      ],
     },
     {
       question: 'コロンビア',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1eQOx81C5e5CoESYsEnrE9sWhCkTMDPEq/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1eQOx81C5e5CoESYsEnrE9sWhCkTMDPEq/view',
+        }
+      ],
     },
     {
       question: 'ギリシャ',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1n8YZTb3bAqLLL5ygVE8-MstdzECcKJ_b/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1n8YZTb3bAqLLL5ygVE8-MstdzECcKJ_b/view',
+        }
+      ],
     },
     {
       question: 'メキシコ',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1Qr3cFttfhjUoKxLcQrVEhsuO-OJ6VE0_/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1Qr3cFttfhjUoKxLcQrVEhsuO-OJ6VE0_/view',
+        }
+      ],
     },
     {
       question: 'ペルー',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1UgN2Bu2XIvv2YirLRYloSPlbYln_uzf9/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1UgN2Bu2XIvv2YirLRYloSPlbYln_uzf9/view',
+        }
+      ],
     },
     {
       question: 'アメリカ合衆国',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+        }
+      ],
     },
     {
       question: 'ヨーロッパ',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1EB_9q8nSxvJXWMtCfxCWmisM-n03TJYz/view',
+        }
+      ],
     },
     {
       question: '日本',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/11RTMmnHW4SqHNf7htr3xuSi-o2v2vWlp/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/11RTMmnHW4SqHNf7htr3xuSi-o2v2vWlp/view',
+        }
+      ],
     },
     {
       question: 'その他の国々',
-      answer: 'プライバシーポリシー',
-      link: 'https://drive.google.com/file/d/1fUIe6FBss6WrwsrssVr6OM_2uFFVhESD/view',
+      answer:[
+        {
+          type: 'link',
+          text: 'プライバシーポリシー',
+          href: 'https://drive.google.com/file/d/1fUIe6FBss6WrwsrssVr6OM_2uFFVhESD/view',
+        }
+      ],
     },
   ],
 };
