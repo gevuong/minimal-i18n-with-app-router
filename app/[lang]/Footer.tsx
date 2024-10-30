04b8c6fb6bc61b67e88561ba5549ec75011862a7
Anthony Wong <awong88@berkeley.edu>
2024-10-29 20:28:46 -0700

    refactored modals, social links, and disclaimer, into components

--- app/[lang]/Footer.tsx
+++ app/[lang]/Footer.tsx
@@ -2,12 +2,6 @@
 
 import { type Locale } from '@/i18n-config';
 import { basePath } from '@/next.config.mjs';
-import {
-  InstagramIconDarkTurquoise,
-  InstagramIconWhite,
-  LinkedInIconDarkTurquoise,
-  LinkedInIconWhite,
-} from '@/public/images/footer/index';
 import VirufyLogo from '@/public/logos/virufy.svg';
 import ExportedImage from 'next-image-export-optimizer';
 import Link from 'next/link';
@@ -21,9 +15,11 @@ import {
   useState,
 } from 'react';
 import { usei18n } from '../i18n';
-import AccordionItem from './components/AccordionItem';
-import Text from './components/Text';
-import Title from './components/Title';
+import PrivacyPolicyModal from './components/footer/PrivacyPolicyModal';
+import SellMyInformationModal from './components/footer/SellMyInformationModal';
+import CookiePolicyModal from './components/footer/CookiesPolicyModal';
+import SocialLinks from './components/footer/SocialLinks';
+import NonProfitDisclaimers from './components/footer/NonProfitDisclaimers';
 
 const Footer = ({ lang }: { lang: Locale }) => {
   const {
@@ -113,133 +109,31 @@ const Footer = ({ lang }: { lang: Locale }) => {
     <>
       <div>
         {showModalPrivacyPolicy ? (
-          <>
-            <div className="fixed inset-0 z-[111] flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-              <div className="relative mx-auto h-[500px] w-[300px] md:w-[500px] lg:w-[1000px]">
-                {/*content*/}
-                <div className="relative flex w-full flex-col rounded-xl border-0 bg-gradient-to-b from-[#010101] to-[#244D7E] shadow-lg outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex w-full rounded-t border-b border-solid border-slate-200 text-white">
-                    <Title
-                      H="h5Modals"
-                      Text={privacyPolicy.title}
-                      TitleClassProps={'mt-[30px] mx-auto mb-[30px]'}
-                    />
-                    <div className="absolute flex w-full flex-col items-end p-6">
-                      <button
-                        className="flex h-[35px] w-[35px] justify-center px-1 py-2 text-white transition-all duration-150 ease-linear"
-                        type="button"
-                        onClick={() => setShowModalPrivacyPolicy(false)}
-                      >
-                        X
-                      </button>
-                    </div>
-                  </div>
-                  {/*body*/}
-
-                  <div className="flex w-full items-center justify-center">
-                    <div className="w-full px-0">
-                      <div>
-                        {privacyPolicy.body.map((detail, index) => (
-                          <AccordionItem {...detail} lang={lang} key={index} />
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                  {/*footer*/}
-                </div>
-              </div>
-            </div>
-
-            <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-          </>
+          <PrivacyPolicyModal
+            lang={lang}
+            privacyPolicy={privacyPolicy}
+            setShowModalPrivacyPolicy={setShowModalPrivacyPolicy}
+          />
         ) : null}
       </div>
       {/* _____________________ */}
       <div>
         {showModalMyInformation ? (
-          <>
-            <div className="fixed inset-0 z-[111] flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-              <div className="relative mx-auto h-[500px] w-[300px] md:w-[500px] lg:w-[1000px]">
-                {/*content*/}
-                <div className="relative flex w-full flex-col rounded-xl border-0 bg-gradient-to-b from-[#010101] to-[#244D7E] shadow-lg outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex w-full rounded-t border-b border-solid border-slate-200 text-white">
-                    <Title
-                      H="h5Modals"
-                      Text={personalInfo.title}
-                      TitleClassProps={
-                        'mt-[80px] text-center md:mt-[30px] md:mx-auto mb-[30px]'
-                      }
-                    />
-                    <div className="absolute flex w-full flex-col items-end p-6 md:p-1 lg:p-6">
-                      <button
-                        className="flex h-[35px] w-[35px] justify-center px-1 py-2 text-white outline-none transition-all duration-150 ease-linear"
-                        type="button"
-                        onClick={() => setShowModalMyInformation(false)}
-                      >
-                        X
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className="flex w-full items-center justify-center">
-                    <div className="w-full px-0">
-                      {personalInfo.body.map((detail, index) => (
-                        <AccordionItem {...detail} lang={lang} key={index} />
-                      ))}
-                    </div>
-                  </div>
-                  {/*footer*/}
-                </div>
-              </div>
-            </div>
-
-            <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-          </>
+          <SellMyInformationModal
+            lang={lang}
+            personalInfo={personalInfo}
+            setShowModalMyInformation={setShowModalMyInformation}
+          />
         ) : null}
       </div>
       {/* _____________________ */}
       <div>
         {showModalCookiesPolicy ? (
-          <>
-            <div className="fixed inset-0 z-[111] flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-              <div className="relative mx-auto h-[500px] w-[300px] md:w-[500px] lg:w-[1000px]">
-                {/*content*/}
-                <div className="relative flex w-full flex-col rounded-xl border-0 bg-gradient-to-b from-[#010101] to-[#244D7E] shadow-lg outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex w-full rounded-t border-b border-solid border-slate-200 text-white">
-                    <Title
-                      H="h5Modals"
-                      Text={cookiePolicy.title}
-                      TitleClassProps={'mt-[30px] mx-auto mb-[30px]'}
-                    />
-                    <div className="absolute flex w-full flex-col items-end p-6">
-                      <button
-                        className="flex h-[35px] w-[35px] justify-center px-1 py-2 text-white outline-none transition-all duration-150 ease-linear"
-                        type="button"
-                        onClick={() => setShowModalCookiesPolicy(false)}
-                      >
-                        X
-                      </button>
-                    </div>
-                  </div>
-                  {/*body*/}
-
-                  <div className="flex w-full items-center justify-center">
-                    <div className="w-full px-0">
-                      {cookiePolicy.body.map((detail, index) => (
-                        <AccordionItem {...detail} lang={lang} key={index} />
-                      ))}
-                    </div>
-                  </div>
-                  {/*footer*/}
-                </div>
-              </div>
-            </div>
-
-            <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-          </>
+          <CookiePolicyModal
+            lang={lang}
+            cookiePolicy={cookiePolicy}
+            setShowModalCookiesPolicy={setShowModalCookiesPolicy}
+          />
         ) : null}
       </div>
       <footer className="bg-black">
@@ -288,94 +182,8 @@ const Footer = ({ lang }: { lang: Locale }) => {
               </Fragment>
             ))}
           </li>
-          <div className="mt-8 flex justify-center gap-10 lg:mt-4 lg:gap-7">
-            <Link
-              href="https://www.instagram.com/virufy/"
-              className="lg:hidden"
-            >
-              <ExportedImage
-                src={InstagramIconWhite}
-                alt="Instagram icon"
-                className="h-[40px] w-[30px]"
-                basePath={basePath}
-              />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/company/virufy/"
-              className="lg:hidden"
-            >
-              <ExportedImage
-                src={LinkedInIconWhite}
-                alt="LinkedIn icon"
-                className="h-[40px] w-[30px]"
-                basePath={basePath}
-              />
-            </Link>
-            <Link
-              href="https://www.instagram.com/virufy/"
-              className="hidden lg:flex"
-            >
-              <ExportedImage
-                src={InstagramIconDarkTurquoise}
-                alt="Instagram icon"
-                className="h-[40px] w-[30px]"
-                basePath={basePath}
-              />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/company/virufy/"
-              className="hidden lg:flex"
-            >
-              <ExportedImage
-                src={LinkedInIconDarkTurquoise}
-                alt="LinkedIn icon"
-                className="h-[40px] w-[30px]"
-                basePath={basePath}
-              />
-            </Link>
-          </div>
-          <div className="align-center mt-10 flex w-full flex-col gap-7 text-white lg:hidden">
-            <div className="align-center m-auto max-w-4xl text-center text-[25px]">
-              <Text Text={nonProfitSection.title} Style="" TextClassProps="" />
-            </div>
-            <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textOne}
-                Style="normal"
-              />
-            </div>
-            <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textTwo}
-                Style="normal"
-              />
-            </div>
-          </div>
-          <div className="align-center mt-10 hidden w-full flex-col gap-7 text-white lg:flex">
-            <div className="align-center m-auto max-w-4xl text-center font-semibold">
-              <Text
-                Text={nonProfitSection.title}
-                Style="small"
-                TextClassProps=""
-              />
-            </div>
-            <div className="align-center m-auto max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textOne}
-                Style="normalCardHome"
-              />
-            </div>
-            <div className="align-center m-auto max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textTwo}
-                Style="normalCardHome"
-              />
-            </div>
-          </div>
+          <SocialLinks />
+          <NonProfitDisclaimers nonProfitSection={nonProfitSection} />
         </div>
       </footer>
     </>
