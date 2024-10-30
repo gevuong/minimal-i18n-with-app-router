23678cfa306f42666b2bdc73d42172f11a0c8a38
Anthony Wong <awong88@berkeley.edu>
2024-10-29 20:38:38 -0700

    refactored footer navbar into component

--- app/[lang]/Footer.tsx
+++ app/[lang]/Footer.tsx
@@ -1,14 +1,9 @@
 'use client';
 
 import { type Locale } from '@/i18n-config';
-import { basePath } from '@/next.config.mjs';
-import VirufyLogo from '@/public/logos/virufy.svg';
-import ExportedImage from 'next-image-export-optimizer';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import {
   type Dispatch,
-  Fragment,
   type SetStateAction,
   useEffect,
   useMemo,
@@ -18,8 +13,7 @@ import { usei18n } from '../i18n';
 import PrivacyPolicyModal from './components/footer/PrivacyPolicyModal';
 import SellMyInformationModal from './components/footer/SellMyInformationModal';
 import CookiePolicyModal from './components/footer/CookiesPolicyModal';
-import SocialLinks from './components/footer/SocialLinks';
-import NonProfitDisclaimers from './components/footer/NonProfitDisclaimers';
+import FooterNavbar from './components/footer/FooterNavbar';
 
 const Footer = ({ lang }: { lang: Locale }) => {
   const {
@@ -136,56 +130,14 @@ const Footer = ({ lang }: { lang: Locale }) => {
           />
         ) : null}
       </div>
-      <footer className="bg-black">
-        <div className="mx-auto flex w-full max-w-screen-xl flex-col p-4 py-10">
-          <div className="absolute mx-12 mt-4 hidden w-56 justify-start lg:flex">
-            <Link href={`/${lang}`} onClick={() => setActiveLink('Home')}>
-              <ExportedImage
-                src={VirufyLogo}
-                alt="Virufy logo"
-                className="h-[48px] w-[160px]"
-                basePath={basePath}
-              />
-            </Link>
-          </div>
-          <ul className="mb-6 hidden flex-wrap text-white lg:flex">
-            <li className="mt-8 flex w-full justify-center space-x-6 align-text-bottom xl:space-x-10">
-              {navbarLinks.map((link, index) => {
-                return (
-                  // footer links
-                  <Link
-                    key={index}
-                    className={`${
-                      activeLink === link.label
-                        ? 'solid relative border-b-2 before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-white before:transition-transform before:duration-300 hover:before:origin-left hover:before:scale-x-100'
-                        : 'relative before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-white before:transition-transform before:duration-300 hover:before:origin-left hover:before:scale-x-100'
-                    }`}
-                    href={link.route[0]}
-                    onClick={() => setActiveLink(link.label)}
-                  >
-                    {link.label}
-                  </Link>
-                );
-              })}
-            </li>
-          </ul>
-          <div className="flex w-full">
-            <hr className="mx-auto my-4 hidden h-px w-11/12 rounded border-0 bg-white lg:block" />
-          </div>
-          <li className="flex w-full flex-wrap items-center justify-center space-x-2 text-xs font-semibold text-white sm:text-base lg:my-6 lg:space-x-6 lg:no-underline">
-            {footerPrivacyLinks.map(({ label, showModal }, idx) => (
-              <Fragment key={label}>
-                {idx > 0 && <div className="font-bold lg:hidden">|</div>}
-                <button key={label} onClick={() => showModal(true)}>
-                  {label}
-                </button>
-              </Fragment>
-            ))}
-          </li>
-          <SocialLinks />
-          <NonProfitDisclaimers nonProfitSection={nonProfitSection} />
-        </div>
-      </footer>
+      <FooterNavbar
+        lang={lang}
+        navbarLinks={navbarLinks}
+        nonProfitSection={nonProfitSection}
+        setActiveLink={setActiveLink}
+        activeLink={activeLink}
+        footerPrivacyLinks={footerPrivacyLinks}
+      />
     </>
   );
 };
