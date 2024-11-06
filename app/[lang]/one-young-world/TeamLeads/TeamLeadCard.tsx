6b5422ec362f176833e079bd9efec40f3de63ba2
Anthony Wong <awong88@berkeley.edu>
2024-11-06 08:55:57 -0800

    removed commented code

--- app/[lang]/one-young-world/TeamLeads/TeamLeadCard.tsx
+++ app/[lang]/one-young-world/TeamLeads/TeamLeadCard.tsx
@@ -4,26 +4,20 @@ import ExportedImage from 'next-image-export-optimizer';
 
 const TeamLeadCard = ({ name, texts, image, altText }: TeamLeadCard) => {
   return (
-    <div className="relative text-center text-gray-300 w-40 md:w-[23rem]">
+    <div className="relative w-40 text-center text-gray-300 md:w-[23rem]">
       <ExportedImage
         className="h-[225px] w-40 object-cover md:h-[450px] md:w-[23rem]"
         src={image}
         alt={altText}
         basePath={basePath}
       />
-
-      {/* apply opaque background to dim card */}
-      {/* <div className="absolute bottom-0 left-0 right-0 top-0 bg-black bg-opacity-20"></div> */}
-
       {/* Text Container */}
-      {/* <div className="absolute bottom-0 left-0 right-0 h-24 bg-black bg-opacity-50 px-2 py-2 text-center md:h-40"> */}
-        <h2 className="pb-1 text-xs md:text-2xl pt-4">{name}</h2>
-        {texts.map((text, i) => (
-          <div key={i} className="text-[9px] md:text-lg font-thin">
-            {text}
-          </div>
-        ))}
-      {/* </div> */}
+      <h2 className="pb-1 pt-4 text-xs md:text-2xl">{name}</h2>
+      {texts.map((text, i) => (
+        <div key={i} className="text-[9px] font-thin md:text-lg">
+          {text}
+        </div>
+      ))}
     </div>
   );
 };
