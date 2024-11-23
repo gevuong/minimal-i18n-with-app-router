8538e2287df269431d67074ac9d633935985a3e6
gevuong <gevuong5627@gmail.com>
2024-11-23 15:18:28 -0800

    feat: add arrow icon and right padding

--- app/[lang]/components/LocaleSelect.tsx
+++ app/[lang]/components/LocaleSelect.tsx
@@ -6,6 +6,7 @@ import ExportedImage from 'next-image-export-optimizer';
 import type { StaticImageData } from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
+import ArrowIcon from '../faq/ArrowIcon';
 
 const localeRegex = new RegExp(`^/(${i18n.locales.join('|')})`);
 
@@ -53,7 +54,7 @@ const LocaleSelect = () => {
   }, []);
 
   return (
-    <div className="relative inline-block" ref={dropdownRef}>
+    <div className="relative inline-block lg:pr-6" ref={dropdownRef}>
       <button
         onClick={toggleDropdown}
         className={`flex w-40 items-center bg-transparent p-3 text-sm text-white hover:font-bold focus:outline-none lg:w-12 lg:border-none lg:px-0 ${
@@ -65,23 +66,9 @@ const LocaleSelect = () => {
           alt={`${locale} flag`}
           className="ml-10 mr-2 h-5 w-5 lg:ml-2"
         />
-        {locale.toUpperCase()} 
-        <svg
-          className="ml-2 h-4 w-4"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
+        {locale.toUpperCase()}
+        <ArrowIcon isActive={dropdownOpen} />
       </button>
-
       {dropdownOpen && (
         <div className="absolute z-10 w-40 border-b border-l border-r border-gray-500 bg-black pb-2 shadow-md shadow-gray-500 lg:-left-2 lg:mt-0 lg:w-28 lg:border-none lg:bg-transparent lg:shadow-none">
           {i18n.locales.map((localeOption) => (
