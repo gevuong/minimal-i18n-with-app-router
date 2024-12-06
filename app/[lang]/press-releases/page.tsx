d26304a1a4502264f61677d1ff70b8eb6270b6bd
Anthony Wong <awong88@berkeley.edu>
2024-12-05 19:35:31 -0800

    edit: publication to press release

--- app/[lang]/press-releases/page.tsx
+++ app/[lang]/press-releases/page.tsx
@@ -42,12 +42,12 @@ const PressReleasesPage = ({
         <section>
           {/* Cards Container */}
           <div className="relative -mb-24 flex flex-col items-center justify-center space-y-8 bg-[#285ba1] px-10 pb-60 pt-28">
-            {pressReleaseCards.map((publication, i) => (
+            {pressReleaseCards.map((pressRelease, i) => (
               <div
                 key={i}
                 className="space-y-4 rounded-xl bg-[#00000060] px-10 py-8 text-left text-white sm:px-16 md:w-[700px] md:space-y-5"
               >
-                <PublicationCard {...publication} />
+                <PublicationCard {...pressRelease} />
               </div>
             ))}
           </div>
