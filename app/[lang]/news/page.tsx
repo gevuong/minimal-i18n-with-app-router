33c23ecf76a5f4124948fe39734caac302b9a413
Anthony Wong <awong88@berkeley.edu>
2024-12-17 14:51:58 -0800

    feat: restyled news pg to have 2 cols

--- app/[lang]/news/page.tsx
+++ app/[lang]/news/page.tsx
@@ -7,11 +7,7 @@ import ExportedImage from 'next-image-export-optimizer';
 import { usei18n } from '../../i18n';
 import PublicationCard from '../publications/PublicationCard';
 
-const NewsPage = ({
-  params: { lang },
-}: {
-  params: { lang: Locale };
-}) => {
+const NewsPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
   const {
     pressReleases: { pressReleaseSection, pressReleaseCards },
   } = usei18n(lang);
@@ -41,11 +37,11 @@ const NewsPage = ({
         {/* Press Releases Section */}
         <section>
           {/* Cards Container */}
-          <div className="relative -mb-24 flex flex-col items-center justify-center space-y-8 bg-[#285ba1] px-10 pb-60 pt-28">
+          <div className="relative -mb-24 flex flex-wrap justify-center gap-8 bg-[#285ba1] px-8 pb-60 pt-28">
             {pressReleaseCards.map((pressRelease, i) => (
               <div
                 key={i}
-                className="space-y-4 rounded-xl bg-[#00000060] px-10 py-8 text-left text-white sm:px-16 md:w-[700px] md:space-y-5"
+                className="space-y-4 rounded-xl bg-[#00000060] px-10 py-8 text-left text-white sm:px-16 lg:basis-1/3"
               >
                 <PublicationCard {...pressRelease} />
               </div>
