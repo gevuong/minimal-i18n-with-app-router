377e3090c999c6ee9ba3abdcf0d929636708cd41
Anthony Wong <awong88@berkeley.edu>
2024-12-20 09:55:37 -0800

    style change

--- app/[lang]/news/NewsCard.tsx
+++ app/[lang]/news/NewsCard.tsx
@@ -1,30 +1,30 @@
 import { type PublicationCard } from '@/app/i18n/types/publications';
 import Link from 'next/link';
 
-const PublicationCard = ({
+const NewsCard = ({
   title,
   date,
   subtext,
   url,
   linkText,
-}: PublicationCard) => {
+}: NewsCard) => {
   return (
-    <div className='flex flex-col justify-between h-full'>
+    <>
       <h3 className="max-w-lg text-xl font-medium md:max-w-md">{title}</h3>
       <p className={`${!subtext ? 'hidden' : ''}`}>{subtext}</p>
-      <div className="items-center space-y-6 md:flex md:justify-between md:space-y-0">
+      <div className="items-center space-y-6 md:flex md:flex-row md:justify-between md:space-y-0">
         <ul className="list-inside list-disc pb-6">
           <li>{date}</li>
         </ul>
         <Link
-          className="rounded-3xl bg-white px-4 py-2 text-[#123d62] sm:px-8 whitespace-nowrap"
+          className="rounded-3xl bg-white px-4 py-2 text-[#123d62] sm:px-8"
           target="_blank"
           href={url}
         >
           {linkText}
         </Link>
       </div>
-    </div>
+    </>
   );
 };
 
